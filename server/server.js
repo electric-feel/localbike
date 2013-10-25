5bbb695672b71028e1fe1a54840a21a636eade7a
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2013-10-26 00:13:04 +0200

    added mongodb skeleton

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
+// get an express app
 var express = require("express");
 var app = express();
 app.use(express.logger());
 
+// connect to mongodb
+var uristring =  process.env.MONGOLAB_URI || 'mongodb://localhost/localbike';
+var mongoose = require('mongoose');
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+// a test method
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+// start app
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
