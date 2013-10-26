6566b7c47c59a9c35e05f9d30f964ecd3ce4d321
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2013-10-26 02:57:03 +0200

    added sone base controller implementation

--- server/controllers/requests.js
+++ server/controllers/requests.js
@@ -1,22 +1,48 @@
 module.exports = function(app) {
 
-  app.get('/requests', function(request, response) {
-    response.send('GET /requests');
+  app.get('/requests', function(req, response) {
+    return RequestModel.find({}, function (err, requests) {
+      if (!err) {
+        return res.send(requests);
+      } else {
+        return console.log(err);
+      }
+    )};
   });
 
-  app.post('/requests', function(request, response) {
-    response.send('POST /requests');
+  app.post('/requests', function(req, response) {
+    var request;
+    request = new RequestModel({
+      made_at: Date.new(),
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      hours req.body.hours,
+    });
+    request.save(function (err) {
+      if (!err) {
+        return console.log("created");
+      } else {
+        return console.log(err);
+      }
+    });
+    return res.send(request);
   });
 
-  app.get('/requests/:id', function(request, response) {
-    response.send('GET /requests/'+request.params.id);
+  app.get('/requests/:id', function(req, response) {
+    return RequestModel.findById(request.params.id, function (err, product) {
+      if (!err) {
+        return res.send(product);
+      } else {
+        return console.log(err);
+      }
+    )};
   });
 
-  app.put('/requests/:id', function(request, response) {
+  app.put('/requests/:id', function(req, response) {
     response.send('PUT /requests');
   });
 
-  app.delete('/requests/:id', function(request, response) {
+  app.delete('/requests/:id', function(req, response) {
     response.send('DELETE /requests');
   });
 
