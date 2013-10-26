fd594fd1ba9f24c3e59c56c13c5fff6ea6baa54b
janfaessler <mail@janfaessler.ch>
2013-10-26 08:21:57 +0200

    asf

--- client/js/views/HomeView.js
+++ client/js/views/HomeView.js
@@ -7,15 +7,15 @@
   			
   			initialize: function(){
       			_.bindAll(this, 'render');
-      			$(this.el).html();
+      			$(this.el).html("");
       			this.render();
     		},
   			
   			render : function() {
 				$(this.el).append('<div id="home">');
-				$(this.el).append('<p><a class="btn btn-primary btn-block">find bike</a></p>');
-				$(this.el).append('<p><a class="btn btn-primary btn-block">my bikes</a></p>');
-				$(this.el).append('<p><a class="btn btn-primary btn-block">profile</a></p>');
+				$(this.el).append('<p><a href="#find" class="btn btn-primary btn-block">find bike</a></p>');
+				$(this.el).append('<p><a href="#bikes" class="btn btn-primary btn-block">my bikes</a></p>');
+				$(this.el).append('<p><a href="#profile" class="btn btn-primary btn-block">profile</a></p>');
 				$(this.el).append('</div>');
 			}
 		});
